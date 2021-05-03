c0e227e3d2398f8aa4138699858f9f6a0e5f71bd
YuXing <frank.xingyu@gmail.com>
2021-05-03 00:06:06 -0400

    priority queue and part of refine

--- src/SketchRefine.py
+++ src/SketchRefine.py
@@ -1,7 +1,15 @@
-from src.utils import OptimizeObjective
+from queue import Queue
+
+from src.utils import OptimizeObjective, GroupAndRepresentationTuple, SimplePQ
+import random
 
 
 class SketchRefine:
+
+    def __init__(self):
+        #
+        # updated by sketch and used by refine
+        self.P = None
 
     def sketch(self, query, load_write_helper):
         #  we firstly build a representation table, where each tuple represent a groups
@@ -18,15 +26,51 @@ class SketchRefine:
         #  (We need to add some new global constraints to ensure that every representative tuple
         #  does not appear in sketch result more times than the size of its group
 
+        # TODO we need to update the self.P, which is a set of utils.GroupAndRepresentationTuple
+
+        # TODO we finally return ps, which is a list of (we will have a new class for this)
+
         a = 1
 
     # Note that this is a recursive function, and it relay on some class variable
-    def refine(self, Q, P, ps):
+    # Q is the package query to evaluate
+    # P is the partition groups, each entry in P is tuple(Group, Group Representation)
+    # S is partitioning groups yet to be refined (initially S = P)
+    # ps is the refining package (initially the result of SKETCH)
+    def refine(self, Q, S, ps):
         # TODO follow the Pseudocode in the paper
-        a = 1
+        failed_groups = []
+        if S is None:
+            raise Exception("call sketch before refine")
+        if len(S) == 0:
+            return ps, failed_groups
+
+        random_S = list(S)
+        random.shuffle(random_S)
+
+        high_priority_queue = Queue()
+        low_priority_queue = Queue()
+        for group in random_S:
+            low_priority_queue.put(group)
+
+        pq = SimplePQ(failed_groups, high_priority_queue, low_priority_queue)
+
+        while True:
+            curr_group = pq.pop()
+            if curr_group is None:
+                # This means the priority queue is empty
+                break
+            if curr_group not in ps:
+                continue
+
+
+
+        return (None, failed_groups)
 
     def sketch_and_refine(self, query, load_write_helper):
         # TODO take all input, call sketch, update class variable
         #  call refine and return the result package
 
         self.sketch(query, load_write_helper)
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import json
 import os
 from enum import Enum
+from heapq import heappush, heappop
+from queue import Queue
+import numpy as np
+import pandas as pd
 
 
 def processInputFile(path_to_input_file):
@@ -14,10 +18,12 @@ def processInputFile(path_to_input_file):
         raise Exception('invalid input file')
     return data
 
+
 class OptimizeObjective(Enum):
     MAXIMIZE = 1
     MINIMIZE = 2
     AVERAGE = 3
+
 
 class LoadAndWrite:
 
@@ -25,8 +31,7 @@ class LoadAndWrite:
         self.partition_dir = args.temp_dir
 
     # This method is used by sketch to get the representation table
-    def getReprecentation(table_name, objective = OptimizeObjective.MAXIMIZE):
-
+    def getReprecentation(table_name, objective=OptimizeObjective.MAXIMIZE):
         # TODO get path to file by table name and self.partition_dir
 
         # TODO read in corresponding csv
@@ -34,6 +39,47 @@ class LoadAndWrite:
         # TODO filter the df to get the representation table and return it
         a = 1
 
-# TODO a helper function which can help us generate the name of partition file
+    # TODO a helper function which can help us generate the name of partition file
 
 
+class SimplePQ:
+
+    # failed_groups is a set
+    # high_pq and low_pq are queue.Queue
+    def __init__(self, failed_groups, high_pq, low_pq):
+        self.failed_groups = failed_groups
+        self.high_pq = high_pq
+        self.low_pq = low_pq
+
+    def prioritize(self, new_failed_groups):
+        for group in new_failed_groups:
+            if group not in self.failed_groups:
+                self.failed_groups.add(group)
+                self.high_pq.put(group)
+
+    def pop(self):
+
+        if not self.high_pq.empty():
+            return self.high_pq.get()
+        else:
+            while not self.low_pq.empty():
+                candidate = self.low_pq.get()
+                if candidate not in self.failed_groups:
+                    return candidate
+        return None
+
+
+class GroupAndRepresentationTuple:
+
+    def __init__(self, df, representation_tuple):
+        self.df = df
+        self.representation_tuple = representation_tuple
+
+    def get_df(self) -> pd.Dataframe:
+        return self.df
+
+    def get_representation_tuple(self) -> np.ndarray:
+        return self.representation_tuple
+
+    def get_titles(self):
+        return self.df.columns
